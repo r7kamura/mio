7508bd069f9874e0c974e42581b5d5d324eb74f6
Ryo NAKAMURA <r7kamura@gmail.com>
2011-04-28 20:33:18 +0900

    ページ読込時自動でフォームにフォーカス

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,19 @@
 $(function(){
-     $("textarea").keyup(function(){
-          var counter = $(this).val().length;
-        $("#countUp").text(counter);
-        if (counter == 0) {
-            $("#countUp").text("0");
-        } else if (counter >= 140) {
-            $("#countUp").css("color","red");
-        } else {
-          $("#countUp").css("color","#666");
-        }
-    });
+  // focus tweet-box
+  var tweetBox = $(".tweetBox textarea");
+  tweetBox && tweetBox.focus();
+
+  // countup tweet-box
+  $(".tweetBox textarea").keyup(function(){
+    var counter = $(this).val().length;
+    $("#countUp").text(counter);
+    if (counter == 0) {
+      $("#countUp").text("0");
+    } else if (counter >= 140) {
+      $("#countUp").css("color","red");
+    } else {
+      $("#countUp").css("color","#666");
+    }
+  });
 });
 
