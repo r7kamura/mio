f268d73fd9e1d65ccaf89ff9d598fbd0834c2e93
Ryo NAKAMURA <r7kamura@gmail.com>
2011-05-02 00:00:39 +0900

    @,&,#について、前に半角スペースが無くても拾うように変更

--- app/helpers/tweet_helper.rb
+++ app/helpers/tweet_helper.rb
@@ -7,21 +7,21 @@ module TweetHelper
   end
 
   def linkify_room(str)
-    if str =~ /(?:\s+|^)&(\w+)(?:\s+|$)/
+    if str =~ /&(\w+)(?:\s+|$)/
       str = str.gsub($~[1], link_to($~[1], :controller => :room, :action => :show, :name => $~[1].delete("&"))).html_safe
     end
     str
   end
 
   def linkify_hash_tag(str)
-    if str =~ /(?:\s+|^)#(\w+)(?:\s+|$)/
+    if str =~ /#(\w+)(?:\s+|$)/
       str = str.gsub($~[1], link_to($~[1], :controller => :hash_tag, :action => :show, :name => $~[1].delete("#"))).html_safe
     end
     str
   end
 
   def linkify_user(str)
-    if str =~ /(?:\s+|^)@(\w+)(?:\s+|$)/
+    if str =~ /@(\w+)(?:\s+|$)/
       str = str.gsub($~[1], link_to($~[1], :controller => :user, :action => :timeline, :name => $~[1].delete("@"))).html_safe
     end
     str
