80d96d4c300e87ea8e7d54affcb1c7c07a34ccef
Ryo NAKAMURA <r7kamura@gmail.com>
2011-05-04 15:07:43 +0900

    Wiki用のPageモデル作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110429122901) do
+ActiveRecord::Schema.define(:version => 20110504060651) do
 
   create_table "favorites", :force => true do |t|
     t.integer  "tweet_id"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(:version => 20110429122901) do
 
   create_table "hash_tags", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
