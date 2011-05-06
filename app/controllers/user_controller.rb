284366e3d0e77cdc5d661d3887a3ea506c163cd2
Ryo NAKAMURA <r7kamura@gmail.com>
2011-05-06 23:27:04 +0900

    ユーザ設定後のリダイレクト先をトップに

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -28,13 +28,7 @@ class UserController < ApplicationController
     user = current_user
     user.profile_image_url = params[:profile_image_url] if params[:profile_image_url]
     user.screen_name = params[:screen_name]
-    if user.save
-      flash[:message] = "successfully updated"
-    else
-      flash[:message] = "error"
-    end
-
-    redirect_to :user_setting
+    redirect_to :tweet_home
   end
 
 end
