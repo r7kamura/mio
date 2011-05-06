c0d0c32d80e3738f88037fb92c92fc2a23fd5346
Ryo NAKAMURA <r7kamura@gmail.com>
2011-05-07 04:22:11 +0900

    fix: 自動更新で挿入されたつぶやきのReply/RTボタンが動かない問題

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,24 @@
+// ReplyとRTボタンをクリックした時の挙動を設定する
+var clickRetweetAndReply = function(){
+  $(".tweet").each(function(){
+    var tweetBox = $(".tweetBox textarea");
+    var text = $(this).find(".body").text();
+    var user = $(this).find(".user a").text();
+    $(this).find(".buttons .retweet").click(function(){
+      tweetBox.text("RT @" + user + " " + text);
+      moveToTweetBox();
+      tweetBox.get(0).setSelectionRange(0, 0);
+    });
+    $(this).find(".reply").click(function(){
+      tweetBox.text("@" + user + " ");
+      moveToTweetBox();
+      var textLength = tweetBox.text().length;
+      tweetBox.get(0).setSelectionRange(textLength, textLength);
+    });
+  });
+};
+
+// 定期的につぶやきを更新して挿入する
 var updateTweetsPeriodically = function(){
    // ページ指定がある場合は自動更新しない
   if ($(location).attr('href').match(/page/)) {
@@ -12,6 +33,7 @@ var updateTweetsPeriodically = function(){
       data: "id=" + id,
       success: function(html){
         $(".tweets ul").prepend(html);
+        clickRetweetAndReply();
       }
     });
   }, interval);
@@ -35,26 +57,13 @@ $(function(){
     }
   });
 
+  clickRetweetAndReply();
+
   // RT & Reply
   var moveToTweetBox = function(){
     $('html,body').animate({ scrollTop: $("#header").offset().top }, 'slow','swing');
     window.setTimeout(function(){ tweetBox.focus(); }, 1000);
   };
-  $(".tweet").each(function(){
-    var text = $(this).find(".body").text();
-    var user = $(this).find(".user a").text();
-    $(this).find(".buttons .retweet").click(function(){
-      tweetBox.text("RT @" + user + " " + text);
-      moveToTweetBox();
-      tweetBox.get(0).setSelectionRange(0, 0);
-    });
-    $(this).find(".reply").click(function(){
-      tweetBox.text("@" + user + " ");
-      moveToTweetBox();
-      var textLength = tweetBox.text().length;
-      tweetBox.get(0).setSelectionRange(textLength, textLength);
-    });
-  });
 
 });
 
